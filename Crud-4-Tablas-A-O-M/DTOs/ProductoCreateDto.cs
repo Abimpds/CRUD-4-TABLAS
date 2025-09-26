85cee6ffa8bd70455019aa0dc70bdec51f0fc1b4
NoSignificantHarassment37 <mateoatehortua1111@gmail.com>
2025-09-26 17:02:48 -0500

    Producto controller terminado

---
+++ Crud-4-Tablas-A-O-M/DTOs/ProductoCreateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Crud_4_Tablas_A_O_M.DTOs
+{
+    public class ProductoCreateDto
+    {
+        public string Nombre { get; set; } = null!;
+        public decimal Precio { get; set; }
+        public int Stock { get; set; }
+    }
+}
---
+++ Crud-4-Tablas-A-O-M/DTOs/ProductoReadDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Crud_4_Tablas_A_O_M.DTOs
+{
+    public class ProductoReadDto
+    {
+        public int ProductoId { get; set; }
+        public string Nombre { get; set; } = null!;
+        public decimal Precio { get; set; }
+        public int Stock { get; set; }
+    }
+}
