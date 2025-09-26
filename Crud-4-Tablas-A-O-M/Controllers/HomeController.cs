8532865113f19daf946cf26869423ede4f64ae89
Abimpds <abimaelcuentapersonalpro@gmail.com>
2025-09-26 14:17:33 -0500

    Agregar .gitignore y limpiar archivos innecesarios

---
+++ Crud-4-Tablas-A-O-M/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Crud_4_Tablas_A_O_M.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Crud_4_Tablas_A_O_M.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
