3f466aaed5fec00dea451f6ea04c99428e47099b
Abimpds <abimaelcuentapersonalpro@gmail.com>
2025-09-26 15:15:23 -0500

    Ultima Clase

--- Crud-4-Tablas-A-O-M/Models/DetalleVenta.cs
+++ Crud-4-Tablas-A-O-M/Models/DetalleVenta.cs
@@ -4,5 +4,15 @@ namespace Crud_4_Tablas_A_O_M.Models
 {
     public class DetalleVenta
     {
+        public int DetalleVentaId { get; set; }
+        public int VentaId { get; set; }
+        public required Venta Venta { get; set; }
+        public int ProductoId { get; set; }
+        public required Producto Producto { get; set; }
+        public int Cantidad { get; set; }
+
+        // Especificar el tipo de columna con precisión y escala
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Subtotal { get; set; }
     }
 }
