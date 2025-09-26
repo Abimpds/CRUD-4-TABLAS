b17197c91bef7cb81446e9ba7253bf082796c05c
NoSignificantHarassment37 <mateoatehortua1111@gmail.com>
2025-09-26 16:18:13 -0500

    Carpetas wwwroot

---
+++ Crud-4-Tablas-A-O-M/Controllers/VentasController.cs
@@ -0,0 +1,6 @@
+﻿namespace Crud_4_Tablas_A_O_M.Controllers
+{
+    public class VentasController
+    {
+    }
+}
--- Crud-4-Tablas-A-O-M/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Crud_4_Tablas_A_O_M.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
