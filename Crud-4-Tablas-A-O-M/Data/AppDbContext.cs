6c7086dd57235c08c8010bfc1d00ce42839a1f49
NoSignificantHarassment37 <mateoatehortua1111@gmail.com>
2025-09-26 15:44:18 -0500

    dbcontext creado

---
+++ Crud-4-Tablas-A-O-M/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Crud_4_Tablas_A_O_M.Models;
+namespace CRUD.ModelsCrud_4_Tablas_A_O_M.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options) { }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<DetalleVenta> DetallesVentas { get; set; }
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<Venta> Ventas { get; set; }
+    }
+}
---
+++ Crud-4-Tablas-A-O-M/Data/DbContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using CRUD.ModelsCrud_4_Tablas_A_O_M.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Crud_4_Tablas_A_O_M.Data
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            // Cargar configuración desde appsettings.json
+            IConfiguration config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = config.GetConnectionString("MySQLConnection");
+
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
+
